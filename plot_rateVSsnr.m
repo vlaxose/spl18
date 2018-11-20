defe33c99e0163c83af04092b23cca690ff1aea9
vlaxose <vlachos.evangelos@gmail.com>
2018-11-20 13:59:51 +0000

    pc@UoE update plot scripts

--- plot_rateVSsnr.m
+++ plot_rateVSsnr.m
@@ -1,39 +1,53 @@
 clear;
 clc;
 
-%%% Initialization
-Mt = 32;
+addpath('basic_system_functions');
+addpath(genpath('benchmark_algorithms'));
+
+%% Parameter initialization
+Mt = 64;
 Mr = Mt;
-total_num_of_clusters = 2;
-total_num_of_rays = 1;
-L = total_num_of_clusters*total_num_of_rays;
+total_num_of_clusters = 2; % number of clusters for the mmWave channel
+total_num_of_rays = 1; % number of rays for the mmWave channel
+L = total_num_of_clusters*total_num_of_rays; % Total number of distinct paths of the mmWave channel
 
-snr_range = [0:10:30];
-subSamplingRatio_range = [0.6];
-Imax = 50;
-maxRealizations = 10;
+snr_range = [0:5:25]; % range of the transmit signal-to-noise ratio
+T_range = [2458]; % training length
+Imax = 50; % maximum number of iterations for the iterative algorithms
+maxMCRealizations = 10;
 
-rate_mcsi = zeros(maxRealizations,1);
-rate_opt = zeros(maxRealizations,1);
-rate_omp = zeros(maxRealizations,1);
-rate_vamp = zeros(maxRealizations,1);
-rate_twostage = zeros(maxRealizations,1);
+%% Variables initialization
 
-mean_rate_mcsi = zeros(length(subSamplingRatio_range), length(snr_range));
+rate_proposed = zeros(maxMCRealizations,1);
+rate_opt = zeros(maxMCRealizations,1);
+rate_omp = zeros(maxMCRealizations,1);
+rate_vamp = zeros(maxMCRealizations,1);
+rate_twostage = zeros(maxMCRealizations,1);
+mean_rate_proposed = zeros(length(subSamplingRatio_range), length(snr_range));
 mean_rate_opt =  zeros(length(subSamplingRatio_range), length(snr_range));
 mean_rate_omp =  zeros(length(subSamplingRatio_range), length(snr_range));
 mean_rate_vamp =  zeros(length(subSamplingRatio_range), length(snr_range));
 mean_rate_twostage =  zeros(length(subSamplingRatio_range), length(snr_range));
 
+Dr = 1/sqrt(Mr)*exp(-1j*[0:Mr-1]'*2*pi*[0:Mr-1]/Mr);
+Dt = 1/sqrt(Mt)*exp(-1j*[0:Mt-1]'*2*pi*[0:Mt-1]/Mt);
+B = kron(conj(Dt), Dr);
+
+
+%% Iterations for different SNRs, training length and MC realizations
 for snr_indx = 1:length(snr_range)
   snr = 10^(-snr_range(snr_indx)/10);
 
-  for sub_indx=1:length(subSamplingRatio_range)
-
-   parfor r=1:maxRealizations
+  for sub_indx=1:length(T_range)
+   T = T_range(sub_indx);
+   
+   parfor r=1:maxMCRealizations
    disp(['realization: ', num2str(r)]);
 
-    [H,Ar,At] = parametric_mmwave_channel(Mr, Mt, total_num_of_clusters, total_num_of_rays);
+    % Create the mmWave MIMO channel
+    [H,Ar,At] = generate_mmwave_channel(Mr, Mt, 1, Np);
+    
+
     [Uh,Sh,Vh] = svd(H);
     rate_opt(r) = log2(real(det(eye(Mr)+1/(Mt*Mr)*1/snr*H*H')));
     
@@ -55,12 +69,12 @@ for snr_indx = 1:length(snr_range)
     [U_omp, S_omp, V_omp] = svd(X_omp);
     rate_omp(r) = log2(real(det(eye(Mr) + 1/(Mt*Mr)*H*H'*1/(snr+norm(H-X_omp)^2/norm(H)^2))));
     
-    % ADMM matrix completion with side-information
+    % Proposed technique based on ADMM matrix completion with side-information
     rho = 0.005;
     tau_S = .1/(1+snr_range(snr_indx));
     X_mcsi = mcsi_admm(H, OH, Omega, Fr, Ft, Imax, rho*norm(OH), tau_S, rho, 1);
     [U_mcsi,S_mcsi,V_mcsi] = svd(X_mcsi);
-    rate_mcsi(r) = log2(real(det(eye(Mr) + 1/(Mt*Mr)*H*H'*1/(snr+norm(H-X_mcsi)^2/norm(H)^2))));
+    rate_proposed(r) = log2(real(det(eye(Mr) + 1/(Mt*Mr)*H*H'*1/(snr+norm(H-X_mcsi)^2/norm(H)^2))));
     
     % Two-stage scheme matrix completion and sparse recovery
     X_twostage_1 = mc_svt(H, OH, Omega, Imax);
@@ -70,7 +84,7 @@ for snr_indx = 1:length(snr_range)
     rate_twostage(r) = log2(real(det(eye(Mr) + 1/(Mt*Mr)*H*H'*1/(snr+norm(H-X_twostage)^2/norm(H)^2))));
    end
 
-    mean_rate_mcsi(sub_indx, snr_indx) = mean(rate_mcsi);
+    mean_rate_proposed(sub_indx, snr_indx) = mean(rate_proposed);
     mean_rate_omp(sub_indx, snr_indx) = mean(rate_omp);
     mean_rate_opt(sub_indx, snr_indx) = mean(rate_opt);
     mean_rate_vamp(sub_indx, snr_indx) = mean(rate_vamp);
@@ -85,15 +99,15 @@ figure;
 p11 = plot(snr_range, (mean_rate_omp(1, :)));hold on;
 set(p11,'LineWidth',2, 'LineStyle', '-', 'MarkerEdgeColor', 'Black', 'MarkerFaceColor', 'Black', 'Marker', '>', 'MarkerSize', 8, 'Color', 'Black');
 p12 = plot(snr_range, (mean_rate_twostage(1, :)));hold on;
-set(p12,'LineWidth',2, 'LineStyle', '-', 'MarkerEdgeColor', 'Cyan', 'MarkerFaceColor', 'Cyan', 'Marker', 's', 'MarkerSize', 8, 'Color', 'Cyan');
+set(p12,'LineWidth',2, 'LineStyle', '-', 'MarkerEdgeColor', 'Black', 'MarkerFaceColor', 'Black', 'Marker', 's', 'MarkerSize', 8, 'Color', 'Black');
 p15 = plot(snr_range, (mean_rate_vamp(1, :)));hold on;
 set(p15,'LineWidth',2, 'LineStyle', '-', 'MarkerEdgeColor', 'Blue', 'MarkerFaceColor', 'Blue', 'Marker', 'o', 'MarkerSize', 8, 'Color', 'Blue');
-p14 = plot(snr_range, (mean_rate_mcsi(1, :)));hold on;
+p14 = plot(snr_range, (mean_rate_proposed(1, :)));hold on;
 set(p14,'LineWidth',2, 'LineStyle', '-', 'MarkerEdgeColor', 'Green', 'MarkerFaceColor', 'Green', 'Marker', 'h', 'MarkerSize', 8, 'Color', 'Green');
 p16 = plot(snr_range, (mean_rate_opt(1, :)));hold on;
 set(p16,'LineWidth',2, 'LineStyle', '--', 'Color', 'Black');
 
-legend({'OMP [19]', 'Two-stage scheme [21]', 'VAMP [20]', 'Proposed', 'Perfect CSI'}, 'FontSize', 12);
+legend({'OMP [4]', 'TSSR [9]', 'VAMP [12]', 'Proposed', 'Perfect CSI'}, 'FontSize', 12);
 
 xlabel('SNR (dB)');
 ylabel('ASE')
